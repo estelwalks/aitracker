13c77cf54b342afccfe812a14c50dd605c59519d
estelwalks <estelwalks@gmail.com>
2026-08-07 09:58:44 +0800

    feat(sources): add health projection query

---
+++ src/modules/sources/index.ts
@@ -0,0 +1,14 @@
+export { sourcesModuleId } from "./contracts.ts";
+export type {
+  GetSourceHealthRequest,
+  SourceFreshness,
+  SourceHealth,
+  SourceHealthInputs,
+  SourceHealthRepository,
+  SourceHealthSnapshot,
+  SourceHealthStatus,
+  SourcesApplication,
+  SourcesApplicationErrorCode,
+  SourcesModuleId,
+} from "./contracts.ts";
+export { createSourcesApplication } from "./application/index.ts";
